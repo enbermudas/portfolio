32a4ddf3349d98d2a72eda71fbb984b4114a59ae
Enrique Bermúdez <enbermudez96@gmail.com>
2022-10-02 17:33:12 -0300

    feat: define theme colors and font

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import theme from "../src/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+const theme = {
+  fontFamily: {
+    ms: "MS sans-serif"
+  },
+  color: {
+    grayPrimary: "#BFBFBF",
+    graySecondary: "#808080",
+    white: "#FFFFFF",
+    black: "#000000",
+    desktopBg: "#008080",
+    windowTitle: {
+      grayDark: "#7B7B7B",
+      grayLight: "#ADADAD",
+      blueDark: "#00007B",
+      blueLight: "#1085D2",
+    }
+  }
+};
+
+export default theme;

c2c32e59f1f7a40610867af503714b0b05d0be80
Enrique Bermúdez <enbermudez96@gmail.com>
2022-10-02 22:33:25 -0300

    fix: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,7 @@ import Window from "./components/Window";
 const App = () => {
   return (
     <div className="App">
-      <Window
-        title="React Fun"
-        content="Warning! You are about to blend the whole world! I'm just trying to fill the whole window."
-        onMinimize={() => {}}
-        onMaximize={() => {}}
-        onClose={() => {}}
-        onSubmit={() => {}}
-        submitText="Bend"
-        onCancel={() => {}}
-        cancelText="Cancel"
-        inactive
-      />
+      App
     </div>
   );
 }

8cbea3e55b9b7e9ba005d46ea11faeb4ed46f757
Enrique Bermúdez <enbermudez96@gmail.com>
2022-10-06 00:08:22 -0300

    feat: move locales location

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,8 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import translationEN from "../src/locales/en/translation.json";
+import translationES from "../src/locales/es/translation.json";
 
 i18n
   .use(Backend)
@@ -9,7 +11,15 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
-    debug: true,
+    debug: false,
+    resources: {
+      en: {
+        translation: translationEN
+      },
+      es: {
+        translation: translationES
+      }
+    },
     interpolation: {
       escapeValue: false,
     }
